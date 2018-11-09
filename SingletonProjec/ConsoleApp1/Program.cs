5528021330dc365d3732ee6e613d6d63e114032c
muharremercan <muharrem.ercan@hotmail.com>
2018-11-09 15:50:09 +0300

    Singleton

---
+++ SingletonProjec/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingletonProjec;
+using SingletonProjec.Model;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.getSingeltonEntitiy();
+            var stList = singleton.getAllStudent();
+            foreach (var item in stList)
+            {
+                Console.WriteLine(item.Name);
+                
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ SingletonProjec/SingletonProjec/Singleton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingletonProjec.Model;
+namespace SingletonProjec
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton SingeltonObject;
+
+        public static Singleton getSingeltonEntitiy()
+        {
+            if (SingeltonObject == null)
+                SingeltonObject = new Singleton();
+            return SingeltonObject;
+
+        }
+
+        public List<Student> getAllStudent()
+        {
+            try
+            {
+
+                using (var db = Model.SchoolEntities.DbEntity())
+                {
+                    var stList = db.Students.ToList();
+                    return stList;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+
+
+    }
+}
